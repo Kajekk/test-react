2751953c0012d9720d2896d9d4682ae3426e3fe6
Vinh Tang <tangquocvinh1000@gmail.com>
2021-01-27 09:13:46 +0700

    Update files

--- todo-react-redux/src/TodoApp.js
+++ todo-react-redux/src/TodoApp.js
@@ -6,24 +6,8 @@ import {VISIBILITY_FILTERS} from "./constants";
 import "./styles.css";
 
 export default function TodoApp() {
-    // const [todos, setTodos] = useState([])
-    // const [currentFilter, setCurrentFilter] = useState(VISIBILITY_FILTERS.ALL)
-    // const handleAddTodo = (todo) => {
-    //     let newArray = [...todos];
-    //     newArray.push({
-    //         id: todos.length ? todos[todos.length - 1].id + 1 : 1,
-    //         content: todo.content,
-    //         status: VISIBILITY_FILTERS.INCOMPLETE
-    //     })
-    //
-    //     setTodos(newArray)
-    // }
-    // const submitFilter = (keyFilter) => {
-    //     setCurrentFilter(keyFilter)
-    // }
     return (
         <div className="todo-app">
-            {/*<h1>Todo List</h1>*/}
             <AddTodo />
             <TodoList />
             <VisibilityFilters />
--- todo-react-redux/src/index.js
+++ todo-react-redux/src/index.js
@@ -7,7 +7,7 @@ import TodoApp from "./TodoApp";
 const rootElement = document.getElementById('root')
 ReactDOM.render(
     <Provider store={store}>
-        <TodoApp />,
+        <TodoApp />
     </Provider>,
     rootElement
 )
